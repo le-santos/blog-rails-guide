3a100f924b9bfd30e212938aa42106d8547a98aa
le-santos <lequinterio@gmail.com>
2021-01-14 11:02:41 -0300

    better comment about associations

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,6 @@
 class Article < ApplicationRecord
   has_many :comments, 
-    dependent: :destroy #destroy the dependent active record associations (e.g. comments)
+    dependent: :destroy # links destroy action to the associated dependents (e.g. comments)
 
   validates :title, presence: true
   validates :body, presence: true, length: { minimum: 10 }
